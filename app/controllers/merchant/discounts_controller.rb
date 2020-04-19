f466562bdb56c3c0d422b8ae3a80450ab18989c6
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-19 15:59:51 -0600

    Add new and create methods to discounts controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -7,4 +7,27 @@ class Merchant::DiscountsController < Merchant::BaseController
   def show
     @discount = Discount.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    merchant = Merchant.find(current_user.merchant_id)
+    discount = merchant.discounts.new(discount_params)
+
+    if discount.save
+      flash[:success] = "Your bulk discount has been created!"
+      redirect_to merchant_discounts_path
+    else
+      flash.now[:error] = discount.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def discount_params
+    params.permit(:percent_off, :min_quantity)
+  end
+
 end
