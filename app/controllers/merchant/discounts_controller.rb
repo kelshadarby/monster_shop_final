722179da8651842cc641a4ba35e476587df122fd
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-18 20:12:42 -0600

    Create show method

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,6 +1,10 @@
 class Merchant::DiscountsController < Merchant::BaseController
   def index
-    merchant = Merchant.find(current_user.merchant_id)
-    @discounts = merchant.discounts.all if current_merchant_user?
+    @merchant = Merchant.find(current_user.merchant_id)
+    @discounts = @merchant.discounts.all if current_merchant_user?
+  end
+
+  def show
+    @discount = Discount.find(params[:id])
   end
 end
